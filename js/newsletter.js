763cfbbaf14e078c38e35c06d8bc5f8af632a685
Usuario invitado <Guest@MacBook-Air-de-Stephanie.local>
2018-02-07 22:28:47 -0600

    Estilos y funcoinalidad

---
+++ js/newsletter.js
@@ -0,0 +1,39 @@
+var datos_contacto ={
+  "correo":"",
+  "nombre": "",
+  "perros":[]
+  };
+$(document).ready(function(){
+  $("#fila-2, #fila-3, #fila-4, #fila-5").hide();
+  //input de correo
+  $('input#correo').on('keyup', function(){
+    datos_contacto.correo = $('input#correo').val().trim();
+    if( datos_contacto.correo ){
+      $('#fila-2').show();
+    }else {
+      $('#fila-2').hide();
+    }
+  });
+
+  //input de nombre
+  $('input#nombre').on('keyup', function(){
+    datos_contacto.nombre = $('input#nombre').val().trim();
+    if( datos_contacto.nombre ){
+      $('#fila-3').show();
+    }else {
+      $('#fila-3').hide();
+    }
+  });
+
+  //input de numero
+  $('input#numero').on('keyup', function(){
+    var numero = parseInt( $('input#numero').val().trim());
+    if( numero > 0 ){
+      $('#fila-4').show();
+    }else {
+      $('#fila-4').hide();
+    }
+  });
+
+
+});
