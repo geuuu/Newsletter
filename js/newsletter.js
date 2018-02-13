d63147f5d39b0e5c82fffc8fcb390945e9437efd
Usuario invitado <Guest@MacBook-Air-de-Stephanie.local>
2018-02-12 22:12:55 -0600

    Casi completo

--- js/newsletter.js
+++ js/newsletter.js
@@ -5,6 +5,12 @@ var datos_contacto ={
   };
 $(document).ready(function(){
   $("#fila-2, #fila-3, #fila-4, #fila-5").hide();
+  $('#formulario').on('submit',function(event){
+    event.preventDefault();
+  })
+  $('#fila-4').delegate('input', 'keyup', function(){
+
+  });
   //input de correo
   $('input#correo').on('keyup', function(){
     datos_contacto.correo = $('input#correo').val().trim();
@@ -29,6 +35,20 @@ $(document).ready(function(){
   $('input#numero').on('keyup', function(){
     var numero = parseInt( $('input#numero').val().trim());
     if( numero > 0 ){
+
+      $('#fila-4').html('');
+        var indice = 0;
+        while( indice < numero) {
+          $('#fila-4').append(
+
+            '<div class="col">'+
+              '<p>Se llama</p>'+
+              '<input type="text" class="form-control">'+
+            '</div>');
+          indice++;
+        }
+
+
       $('#fila-4').show();
     }else {
       $('#fila-4').hide();
